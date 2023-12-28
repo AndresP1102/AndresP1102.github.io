fa70aa76df64dfaf23ebfa19ec957460353b397f
AndresP1102 <89894189+AndresP1102@users.noreply.github.com>
2023-12-28 11:03:27 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -346,19 +346,5 @@ var toggleSound = function toggleSound() {
     on = !on;
   };
 };
-
-// Call the returned function from toggleSound
-var toggle = toggleSound();
-el.sound.addEventListener('click', toggle);
-el.sound.addEventListener('touchstart', toggle);
-
-// Add touchstart event listeners to the body
-document.body.addEventListener('touchstart', function() {
-  // Only play the audios if they're not already playing
-  ['blup', 'blop'].forEach(function(id) {
-    var audio = el[id];
-    if (audio && audio.paused) {
-      audio.play();
-    }
-  });
-});
+el.sound.addEventListener('click', toggleSound);
+el.sound.addEventListener('touchstart', toggleSound);
